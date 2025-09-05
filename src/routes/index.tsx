9bffb0b50d23a24812db9a3a85c34f0ea66bedfc
Davi Santos <daviir17@gmail.com>
2025-09-04 23:26:54 -0300

    inicialização do repositorio

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from 'react-router-dom';
+
+export const RoutesApp = () => {
+  return (
+    <Routes>
+      {/* <Route path="/" element={<AdminDashboard />} /> */}
+    </Routes>
+  );
+};
